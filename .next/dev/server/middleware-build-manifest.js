a23fded520bf421eeb1291c40aa4670fa7e8bed7
Belindq <unicornbelinda@gmail.com>
2026-01-17 09:04:11 -0500

    added backend folder

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,22 +3,9 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/cc9ee_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_80e41786._.js",
-    "static/chunks/cc9ee_next_dist_compiled_react-dom_eebbd7bd._.js",
-    "static/chunks/cc9ee_next_dist_compiled_react-server-dom-turbopack_62e924ad._.js",
-    "static/chunks/cc9ee_next_dist_compiled_next-devtools_index_4c4c18b0.js",
-    "static/chunks/cc9ee_next_dist_compiled_4019009c._.js",
-    "static/chunks/cc9ee_next_dist_client_3bafc2dd._.js",
-    "static/chunks/cc9ee_next_dist_a523cc87._.js",
-    "static/chunks/cc9ee_@swc_helpers_cjs_2fe168db._.js",
-    "static/chunks/Documents_GitHub_LockedIn_a0ff3932._.js",
-    "static/chunks/turbopack-Documents_GitHub_LockedIn_d2b79255._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
